a08b500e11d96880283023664170c96cf26b7ae3
Time_ON <haas.timeon@gmail.com>
2023-11-01 10:14:26 +0100

    Calendar finished | added: -Methods for 30 days and 31 days -Method for Sys.out "Month" -StringArray for days -intArray for daysInMonth -int for offsetDays -switch to determine starting day (offset) -switch for user to select Month -code for Feburary

--- src/main/java/Kalender.java
+++ src/main/java/Kalender.java
@@ -1,6 +1,177 @@
+import java.util.Scanner;
+
 public class Kalender {
+
+    private static void printCalendarForMonth(String month) {
+        System.out.println(month);
+    }
+    private static void print31Days(String[] day,int[] daysInMonth, int offsetDays) {
+        for (int i = 0; i < day.length; i++) {
+            System.out.print(day[i]);
+        }
+        System.out.println("");
+        for (int i = 0; i < offsetDays; i++) {
+            System.out.print("   ");
+        }
+        for (int i = 0; i < 31; i++) {
+            if (daysInMonth[i] >= 10) {
+                System.out.print(daysInMonth[i] + " ");
+            } else if (i == 11) {
+                System.out.print(daysInMonth[i]);
+            } else {
+                System.out.print(daysInMonth[i] + "  ");
+            }
+
+            if (i == (6-offsetDays)) {
+                System.out.println("");
+            } else if (i == (13-offsetDays)) {
+                System.out.println("");
+            } else if (i == (20-offsetDays)) {
+                System.out.println("");
+            } else if (i == (27-offsetDays)) {
+                System.out.println("");
+            } else if (i == (34-offsetDays)) {
+                System.out.println("");
+            }
+        }
+    }
+    private static void print30Days(String[] day,int[] daysInMonth,int offsetDays) {
+        for (int i = 0; i < day.length; i++) {
+            System.out.print(day[i]);
+        }
+        System.out.println("");
+        for (int i = 0; i < offsetDays; i++) {
+            System.out.print("   ");
+        }
+        for (int i = 0; i < 30; i++) {
+            if (daysInMonth[i] >= 10) {
+                System.out.print(daysInMonth[i] + " ");
+            } else if (i == 11) {
+                System.out.print(daysInMonth[i]);
+            } else {
+                System.out.print(daysInMonth[i] + "  ");
+            }
+
+            if (i == (6-offsetDays)) {
+                System.out.println("");
+            } else if (i == (13-offsetDays)) {
+                System.out.println("");
+            } else if (i == (20-offsetDays)) {
+                System.out.println("");
+            } else if (i == (27-offsetDays)) {
+                System.out.println("");
+            } else if (i == (34-offsetDays)) {
+                System.out.println("");
+            }
+        }
+    }
     public static void main(String[] args) {
-        String[] month = {"Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "Juni", "August", "September", "Oktober", "November", "Dezember"};
-        System.out.println(month[1]);
+        String[] month = {"Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"};
+        String[] day = {"Mo ", "Di ", "Mi ", "Do ", "Fr ", "Sa ", "So"};
+        int[] daysInMonth = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31};
+        int offsetDays = 0;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Day on which to start (Mo,Di,Mi,Do,Fr,Sa,So)");
+        String startDay = sc.next();
+
+        switch (startDay) {
+            case "Mo":
+                offsetDays = offsetDays+0;
+                break;
+            case "Di":
+                offsetDays = offsetDays+1;
+                break;
+            case "Mi":
+                offsetDays = offsetDays+2;
+                break;
+            case "Do":
+                offsetDays = offsetDays+3;
+                break;
+            case "Fr":
+                offsetDays = offsetDays+4;
+                break;
+            case "Sa":
+                offsetDays = offsetDays+5;
+                break;
+            case "So":
+                offsetDays = offsetDays+6;
+                break;
+            default:
+                System.out.println("Invalid input");
+        }
+
+        if (offsetDays <= 6 && offsetDays >= 0) {
+            System.out.println("Enter the number of the month you want to display");
+            int user_input = sc.nextInt();
+            user_input = user_input - 1;
+            printCalendarForMonth(month[user_input]);
+
+            switch (user_input) {
+                case 0:
+                    print31Days(day, daysInMonth, offsetDays);
+                    break;
+                case 1:
+                    for (int i = 0; i < day.length; i++) {
+                        System.out.print(day[i]);
+                    }
+                    System.out.println("");
+                    for (int i = 0; i < offsetDays; i++) {
+                            System.out.print("   ");
+                    }
+                    for (int i = 0; i < 28; i++) {
+                        if (daysInMonth[i] >= 10) {
+                            System.out.print(daysInMonth[i] + " ");
+                        } else if (i == 11) {
+                            System.out.print(daysInMonth[i]);
+                        } else {
+                            System.out.print(daysInMonth[i] + "  ");
+                        }
+
+                        if (i == (6-offsetDays)) {
+                            System.out.println("");
+                        } else if (i == (13-offsetDays)) {
+                            System.out.println("");
+                        } else if (i == (20-offsetDays)) {
+                            System.out.println("");
+                        } else if (i == (27-offsetDays)) {
+                            System.out.println("");
+                        }
+                    }
+                    break;
+                case 2:
+                    print31Days(day, daysInMonth, offsetDays);
+                    break;
+                case 3:
+                    print30Days(day, daysInMonth, offsetDays);
+                    break;
+                case 4:
+                    print31Days(day, daysInMonth, offsetDays);
+                    break;
+                case 5:
+                    print30Days(day, daysInMonth, offsetDays);
+                    break;
+                case 6:
+                    print31Days(day, daysInMonth, offsetDays);
+                    break;
+                case 7:
+                    print30Days(day, daysInMonth, offsetDays);
+                    break;
+                case 8:
+                    print31Days(day, daysInMonth, offsetDays);
+                    break;
+                case 9:
+                    print30Days(day, daysInMonth, offsetDays);
+                    break;
+                case 10:
+                    print31Days(day, daysInMonth, offsetDays);
+                    break;
+                case 11:
+                    print30Days(day, daysInMonth, offsetDays);
+                    break;
+                default:
+                    System.out.println("Invalid input");
+            }
+        }
     }
 }
